faf2116cc5e072aab47d8ac2993546f968b13f43
Hannah C <hannah.cloves@outlook.com>
2020-10-18 15:16:24 +0100

    added comments to JS

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
+// JS runs as page loads
 $(document).ready(function () {
 
     //Sets time at top of page
     $("#currentDay").append(moment().format("MMMM Do YYYY"));
 
-
+    // variables for functions
+    const saveBtn = $(".saveBtn");
     let currentTime = parseInt(moment().format("HH"));
 
+    // loops through the current time vs the time on the planner and assigns classes depending on the outcome
     $(".dataEntry").each(function () {
         let schedulerHour = parseInt($(this).attr("id"));
-
         if (currentTime > schedulerHour) {
             $(this).addClass("past");
         }
@@ -20,7 +22,7 @@ $(document).ready(function () {
         }
     })
 
-    const saveBtn = $(".saveBtn");
+    // When save button is clicked, input entries and time are stored in local storage
     saveBtn.on('click', function () {
         let value = $(this).siblings(".dataEntry").val();
         console.log(value)
@@ -30,6 +32,7 @@ $(document).ready(function () {
 
     });
 
+    // Stored items are shown in the input 
     $(".hour-8").val(localStorage.getItem("8"));
     $(".hour-9").val(localStorage.getItem("9"));
     $(".hour-10").val(localStorage.getItem("10"));
